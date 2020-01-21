dec5a7f77d396a8a655c3e3160f93ab0faeff9be
Fatih Gürdal <f.gurdal@hotmail.com.tr>
2020-01-21 17:47:23 +0300

    console

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,16 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
+using AuthorizationService1;
 using Grpc.Net.Client;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
 
-            //var channel = GrpcChannel.ForAddress("");
-            //var client = new AuthorizationService.cli
+
+            var channel = GrpcChannel.ForAddress("https://localhost:4001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var userName = "";
+            var password = "";
+
+            while (userName != "exit")
+            {
+                Console.Write("User Name: ");
+                userName = Console.ReadLine();
+                Console.Write("Password: ");
+                password = Console.ReadLine();
+
+                var input = new LoginRequest() { Password = password, Username = userName };
+                var result = await client.LoginAsync(input);
+
+                Console.WriteLine(result.Verification);
+                Console.WriteLine(result.Message);
+            }
+       
+
+            Console.ReadLine();
         }
     }
 }
